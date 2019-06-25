3d765c6093b9864375cdabf33a4cf02addf605a4
Guillermo Mora <52202286+geemoe@users.noreply.github.com>
2019-06-25 20:51:09 +0900

    Hello world
    This is my hello world java file.

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+ * Print "My First Hello!" to standard output.
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) { 
+        	System.out.println("Hello!");
+		System.out.println("My name is Guillermo.");
+        }
+}
